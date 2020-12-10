015f298c30d0b0a9f4b68d4e879e5706a7c60ce0
Johanna Horn <j.von.horn@hotmail.com>
2020-12-10 16:12:24 +0100

    setting todo in key for having no error in console

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -5,7 +5,7 @@ class Todos extends Component {
     render() {
         // console.log(this.props.todos)
         return this.props.todos.map((todo) => (
-            <TodoItem todo={todo} />
+            <TodoItem key={todo.id} todo={todo} />
         ));
     }
 }
