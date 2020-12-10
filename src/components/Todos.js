efc076f31c335269d50ff58d42d47139a34389e8
Johanna Horn <j.von.horn@hotmail.com>
2020-12-10 15:21:35 +0100

    making class instead of a f() in App.js + Todos.js

---
+++ src/components/Todos.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+/* function App() {
+  return (
+    <div className="App">
+      <h1>App</h1>
+    </div>
+  );
+} */
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>App </h1>
+            </div>
+        );
+    }
+}
+
+export default App;
